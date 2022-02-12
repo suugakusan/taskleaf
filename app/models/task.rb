b904bea7ea41b9204887ca82f31d45b01bc1d8c6
kojikoji <rubypra0804@gmail.com>
2022-02-12 10:00:24 +0900

    csvの出力完了

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,21 @@ end
 def self.ransackable_associations(auth_object = nil)
   []
 end
+
+def self.csv_attributes
+  ["name", "description", "created_at", "updated_at"]
+end
+
+def self.generate_csv
+  bom = "\uFEFF"
+  CSV.generate(bom) do |csv|
+    csv << csv_attributes
+    all.each do |task|
+      csv << csv_attributes.map{|attr| task.send(attr)}
+    end
+  end
+end
+
   scope :recent, -> { order(created_at: :desc) }
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_conma
